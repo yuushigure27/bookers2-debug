0c229c4bea74fdec93a10089fa8cf1594074270e
EC2 Default User <ec2-user@ip-172-31-7-4.ap-northeast-1.compute.internal>
2024-04-01 05:35:16 +0000

    検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
+   get "/search", to: "searches#search"
   
   devise_for :users
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,16 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
-
-// 下記を追加
-const webpack = require('webpack')
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    Popper: 'popper.js'
-  })
-)
-
-module.exports = environment
